f2983d74877a8676dc21ed86eb27376615e02f09
eitancohen77 <eitancohen77@gmail.com>
2025-09-21 10:44:42 -0400

    Added layouts for cards

--- calisthenics_progression/src/app/planche/page.tsx
+++ calisthenics_progression/src/app/planche/page.tsx
@@ -7,33 +7,108 @@ export default function Home() {
     <>
         <div className="flex min-h-screen items-center justify-center bg-slate-900">
             <div className="flex gap-4">
-              <div className="relative w-64 h-40 bg-white p-2 rounded shadow hover:shadow-xl hover:scale-105 transition-transform overflow-hidden">
+
+
+                <div className="relative w-64 h-40 bg-white p-2 rounded shadow hover:shadow-xl hover:scale-105 transition-transform overflow-hidden group">
                 <Image
                   src='/planche.png'
-                  alt='planche'
+                  alt='Planche Lean'
                   fill
-                  className="object-contain"
+                  className="object-contain transition-opacity duration-500 group-hover:opacity-20"
                   priority
                 />
+
+                <div className="absolute inset-0 flex items-center justify-center">
+                    <p className="text-black text-3xl font-semibold opacity-0 transition-opacity duration-500 group-hover:opacity-100 text-center">
+                    Planche Lean
+                    </p>
+                </div>
               </div>
-              <div className="relative w-64 h-40 bg-white p-2 rounded shadow hover:shadow-xl hover:scale-105 transition-transform overflow-hidden">
+
+
+              <div className="relative w-64 h-40 bg-white p-2 rounded shadow hover:shadow-xl hover:scale-105 transition-transform overflow-hidden group">
                 <Image
-                  src='/handstand.png'
-                  alt='handstand'
+                  src='/planche.png'
+                  alt='Frog Stand'
                   fill
-                  className="object-contain"
+                  className="object-contain transition-opacity duration-500 group-hover:opacity-20"
                   priority
                 />
+
+                <div className="absolute inset-0 flex items-center justify-center">
+                    <p className="text-black text-3xl font-semibold opacity-0 transition-opacity duration-500 group-hover:opacity-100 text-center">
+                    Frog Stand
+                    </p>
+                </div>
               </div>
-              <div className="relative w-64 h-40 bg-white p-2 rounded shadow hover:shadow-xl hover:scale-105 transition-transform overflow-hidden">
+
+            
+            <div className="relative w-64 h-40 bg-white p-2 rounded shadow hover:shadow-xl hover:scale-105 transition-transform overflow-hidden group">
                 <Image
-                  src='/pullup.png'
-                  alt='pullup'
+                  src='/planche.png'
+                  alt='Tuck Planche'
                   fill
-                  className="object-contain"
+                  className="object-contain transition-opacity duration-500 group-hover:opacity-20"
                   priority
                 />
+
+                <div className="absolute inset-0 flex items-center justify-center">
+                    <p className="text-black text-3xl font-semibold opacity-0 transition-opacity duration-500 group-hover:opacity-100 text-center">
+                    Tuck Planche
+                    </p>
+                </div>
               </div>
+
+
+            <div className="relative w-64 h-40 bg-white p-2 rounded shadow hover:shadow-xl hover:scale-105 transition-transform overflow-hidden group">
+                <Image
+                  src='/planche.png'
+                  alt='Tuck Planche Push Up'
+                  fill
+                  className="object-contain transition-opacity duration-500 group-hover:opacity-20"
+                  priority
+                />
+
+                <div className="absolute inset-0 flex items-center justify-center">
+                    <p className="text-black text-3xl font-semibold opacity-0 transition-opacity duration-500 group-hover:opacity-100 text-center">
+                    Tuck Planche Push Up
+                    </p>
+                </div>
+              </div>
+
+
+              <div className="relative w-64 h-40 bg-white p-2 rounded shadow hover:shadow-xl hover:scale-105 transition-transform overflow-hidden group">
+                <Image
+                  src='/planche.png'
+                  alt='Straddle Planche'
+                  fill
+                  className="object-contain transition-opacity duration-500 group-hover:opacity-20"
+                  priority
+                />
+
+                <div className="absolute inset-0 flex items-center justify-center">
+                    <p className="text-black text-3xl font-semibold opacity-0 transition-opacity duration-500 group-hover:opacity-100 text-center">
+                    Straddle Planche
+                    </p>
+                </div>
+              </div>
+
+
+              <div className="relative w-64 h-40 bg-white p-2 rounded shadow hover:shadow-xl hover:scale-105 transition-transform overflow-hidden group ">
+                <Image
+                  src='/planche.png'
+                  alt='Planche'
+                  fill
+                  className="object-contain transition-opacity duration-500 group-hover:opacity-20"
+                  priority
+                />
+
+                <div className="absolute inset-0 flex items-center justify-center">
+                    <p className="text-black text-3xl font-semibold opacity-0 transition-opacity duration-500 group-hover:opacity-100 text-center">
+                    Planche
+                    </p>
+                </div>
+              </div>  
             </div>
         </div>
     </>
